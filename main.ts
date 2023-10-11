a30d02664fb621a904c9f883774e9be6c4b31536
David Gage <dgage@fearless.tech>
2023-10-11 16:06:22 -0400

    wip

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
+import WebSocket from 'ws';
+
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
@@ -7,75 +9,97 @@ interface MyPluginSettings {
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	mySetting: 'ws://localhost:8765'
+}
+
+function open(ws, statusBarItemEl, onMessage) {
+	return function () {
+		console.log("connected to websocket.");
+		statusBarItemEl.setText('Dication Connected');
+		ws.on('message', onMessage);
+	}
+}
+
+function message(ws, app) {
+	return function (data) {
+		console.log("got a message %s", data);
+		const msg = new TextDecoder().decode(data);
+		console.log(msg);
+		const view = app.workspace.getActiveViewOfType(MarkdownView);
+		// getActiveViewOfType will return null if the active view is null,
+		// // or is not of type MarkdownView.
+		if (view) {
+			console.log("trying to write the message");
+			const editor = view.editor;
+			try {
+				editor.replaceSelection(JSON.parse(msg).text);
+				editor.replaceSelection('\n');
+			} catch (error) {
+				console.log("failed to parse message");
+				console.log(error);
+			}
+		}
+	}
+}
+
+function error(ws) {
+	return function () {
+	}
+}
+
+function close(ws) {
+	return function () {
+	}
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	ws: WebSocket;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText('Dication Disconnected');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		// This creates an icon in the left ribbon.
+		const ribbonIconEl = this.addRibbonIcon('mic', 'Sample Plugin', (evt: MouseEvent) => {
+			if (this.ws && this.ws.readyState == this.ws.OPEN) {
+				console.log("closing");
+				this.ws.terminate();
+				return;
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			// Called when the user clicks the icon.
+			try {
+				new Notice('Starting dictation.', 5000);
+				this.ws = new WebSocket(this.settings.mySetting);
+				this.ws.on('open', open(this.ws, statusBarItemEl, message(this.ws, this.app)));
+				this.ws.on('error', function error(err) {
+					console.log(err);
+					statusBarItemEl.setText('Dication failed to connect.');
+				});
+				this.ws.on('close', function close() {
+					statusBarItemEl.setText('Dication Disconnected.');
+				});
+
+			} catch (err) {
+				console.log(err);
+				statusBarItemEl.setText('Dication failed to connect.');
+
 			}
 		});
 
+		// Perform additional things with the ribbon
+		ribbonIconEl.addClass('my-plugin-ribbon-class');
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		//this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -123,15 +147,15 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		.setName('Setting #1')
+		.setDesc('It\'s a secret')
+		.addText(text => text
+			.setPlaceholder('Enter your secret')
+			.setValue(this.plugin.settings.mySetting)
+			.onChange(async (value) => {
+				console.log('Secret: ' + value);
+				this.plugin.settings.mySetting = value;
+				await this.plugin.saveSettings();
+			}));
 	}
 }
